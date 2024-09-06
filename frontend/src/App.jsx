da5a48e36b1692f727d68bbacd33f82b03c93e59
kankit <kankit@kankits-MacBook-Air.local>
2024-09-06 23:35:56 +0530

    Solved the infinite request issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { CreateTodo } from "../components/CreateTodo";
 import { Todos } from "../components/Todos";
@@ -6,15 +6,17 @@ import { Todos } from "../components/Todos";
 function App() {
   const [todos, setTodos] = useState([]);
 
-  fetch("http://localhost:3000/todos").then(async function (res) {
-    const response = await res.json();
-    setTodos(response);
-  });
+  useEffect(() => {
+    fetch("http://localhost:3000/todos").then(async function (res) {
+      const response = await res.json();
+      setTodos(response);
+    });
+  }, []);
 
   return (
     <div>
       <CreateTodo></CreateTodo>
-      <Todos todos={todos}></Todos>
+      <Todos key={todos.id} todos={todos}></Todos>
     </div>
   );
 }
